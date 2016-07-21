8acf1faa84016653a3d2e76d149be67191356078
Marceau Dewilde <m@ceau.be>
2016-07-21 22:23:43 +0200

    Extra demo cases

--- tablefilter-0.9.0.js
+++ tablefilter-0.9.0.js
@@ -16,15 +16,11 @@ var tablefilter = (function(){
 	var filterTimeout;
 	
 	
-	var _log = function(s) {
-		'console' in window && console.log(s);
-	};
-	
 	var _init = function() {
 
 		var list = document.querySelectorAll('[data-tablefilter]');
 		for (var i = 0; i < list.length; ++i) {
-			var filter = new Filter(list[i]);
+			var filter = new _filter(list[i]);
 			if (filter.valid) {
 				filters[filter.id] = filter;
 				filter.run();
@@ -33,6 +29,10 @@ var tablefilter = (function(){
 			}
 		}
 
+	};
+	
+	var _log = function(s) {
+		'console' in window && console.log(s);
 	};
 	
 	/**
@@ -45,7 +45,7 @@ var tablefilter = (function(){
 		if (htmlElementSupported) {
 			return o instanceof HTMLElement; 
 		} else {
-			return typeof o === 'object' && typeof o.tagName === 'string';
+			return typeof o === 'object' && 'tagName' in o;
 		}
 	};
 
@@ -136,7 +136,7 @@ var tablefilter = (function(){
 	 */
 	var _parseToggle = function(node) {
 		var toggle = node.getAttribute('data-tablefilter-toggle');
-		if (typeof toggle === 'undefined' || toggle === null || toggle.length === 0) {
+		if (!toggle || !toggle.length) {
 			return 'tr';
 		} else {
 			return toggle.trim().toLowerCase();
@@ -171,7 +171,7 @@ var tablefilter = (function(){
 	/**
 	 * Count down after triggering event occurred
 	 */
-	Filter.prototype.countdown = function() {
+	_filter.prototype.countdown = function() {
 		if (filterTimeout) {
 			clearTimeout(filterTimeout);
 		}
@@ -183,22 +183,25 @@ var tablefilter = (function(){
 	/**
 	 * Remove this filter and its associated events
 	 */
-	Filter.prototype.destroy = function() {
+	_filter.prototype.destroy = function() {
 		this.undo();
 		this.node.removeEventListener(this.trigger, this.ontrigger);
 		delete filters[this.id];
 	};
 	
-	
+	_filter.prototype.timeout = 750;
+	_filter.prototype.valid = false;
+	_filter.prototype.isByAttribute = false;
+
 	/**
 	 * Define method logic
 	 */
-	Filter.prototype.methods = {};
+	_filter.prototype.methods = {};
 	
 	/**
 	 * Display all rows that contain a given value
 	 */
-	Filter.prototype.methods.contain = function() {
+	_filter.prototype.methods.contain = function() {
 		var rows = this.gather();
 		var regExp = new RegExp(this.node.value, 'i');
 		for (var i = 0; i < rows.length; i++) {
@@ -213,7 +216,7 @@ var tablefilter = (function(){
 	/**
 	 * Display all rows that do not contain a given value
 	 */
-	Filter.prototype.methods.exclude = function() {
+	_filter.prototype.methods.exclude = function() {
 		var regExp = new RegExp(this.node.value, 'i');
 		var rows = this.gather();
 		for (var i = 0; i < rows.length; i++) {
@@ -228,7 +231,7 @@ var tablefilter = (function(){
 	/**
 	 * Display all rows that that have a value equal to or greater than
 	 */
-	Filter.prototype.methods.min = function() {
+	_filter.prototype.methods.min = function() {
 		var rows = this.gather();
 		for (var i = 0; i < rows.length; i++) {
 			var int = this.extractInt(rows[i]);
@@ -243,7 +246,7 @@ var tablefilter = (function(){
 	/**
 	 * Display all rows that that have a value less than or equal to
 	 */
-	Filter.prototype.methods.max = function() {
+	_filter.prototype.methods.max = function() {
 		var rows = this.gather();
 		for (var i = 0; i < rows.length; i++) {
 			var int = this.extractInt(rows[i]);
@@ -255,7 +258,7 @@ var tablefilter = (function(){
 		}
 	};
 	
-	Filter.prototype.run = function() {
+	_filter.prototype.run = function() {
 		console.time('run');
 		if (this.isRemove()) {
 			this.undo();
@@ -270,7 +273,7 @@ var tablefilter = (function(){
 	/**
 	 * Collect all table rows that need to be evaluated by this filter
 	 */
-	Filter.prototype.gather = function() {
+	_filter.prototype.gather = function() {
 		var rows = [];
 		var tables = document.querySelectorAll('table[data-tablefiltered]');
 		for (var i = 0; i < tables.length; i++) {
@@ -287,7 +290,7 @@ var tablefilter = (function(){
 	/**
 	 * Evaluate whether the given row should be filtered by this filter
 	 */
-	Filter.prototype.filtersRow = function(tr) {
+	_filter.prototype.filtersRow = function(tr) {
 		var tds = tr.getElementsByTagName('td');
 		if (tds.length > 0) {
 			for (var i = 0; i < tds.length; i++) {
@@ -303,7 +306,7 @@ var tablefilter = (function(){
 	/**
 	 * Extract the text against which to run this filter
 	 */
-	Filter.prototype.extractText = function(tr) {
+	_filter.prototype.extractText = function(tr) {
 		if (!this.isByAttribute) {
 			return tr.textContent;
 		}
@@ -319,10 +322,10 @@ var tablefilter = (function(){
 		return s;
 	};
 
-	Filter.prototype.extractInt = function(tag) {
+	_filter.prototype.extractInt = function(tag) {
 		return parseInt(this.extractText(tag));
 	};
-	Filter.prototype.isRemove = function() {
+	_filter.prototype.isRemove = function() {
 		if (this.type === 'checkbox') {
 			return !this.node.checked;
 		} else {
@@ -333,14 +336,17 @@ var tablefilter = (function(){
 	/**
 	 * Remove the effects of this filter
 	 */
-	Filter.prototype.undo = function() {
+	_filter.prototype.undo = function() {
 		var rows = this.gather();
 		for (var i = 0; i < rows.length; i++) {
 			this.removeFilterId(rows[i]);
 		}
 	};
 	
-	Filter.prototype.removeFilterId = function(row) {
+	/**
+	 * Removes this filter's id from the given row
+	 */
+	_filter.prototype.removeFilterId = function(row) {
 //		row = this.getTagToToggle(tag);
 //		if (!_isNode(row)) {
 //			return false;
@@ -354,7 +360,11 @@ var tablefilter = (function(){
 			}
 		}
 	};
-	Filter.prototype.addFilterId = function(row) {
+
+	/**
+	 * Removes this filter's id from the given row
+	 */
+	_filter.prototype.addFilterId = function(row) {
 //		tag = this.getTagToToggle(tag);
 //		if (!_isNode(tag)) {
 //			return false;
@@ -371,7 +381,7 @@ var tablefilter = (function(){
 		row.setAttribute('data-tablefilter-ids', ids);		
 	};
 	
-//	Filter.prototype.getTagToToggle = function(tag) {
+//	_filter.prototype.getTagToToggle = function(tag) {
 //		if (!_isNode(tag)) {
 //			return false;
 //		}
@@ -397,34 +407,20 @@ var tablefilter = (function(){
 //	};
 //
 //	
+	
+	
 	/**
-	 * Constructor function for creating Filter instances.
+	 * Constructor function for creating _filter instances.
 	 * @param node : a plain JavaScript DOM element
 	 */
-	function Filter(node) {
+	function _filter(node) {
 
-		this.valid = false;
-
-		if (!_isNode(node)) {
-			_log('tablefilter: argument ' + node + ' is not an element');
-			return false;
-		} else if (!node.hasAttribute('data-tablefilter')) {
-			_log('tablefilter: argument ' + node + ' does not have the data-tablefilter attribute');
-			return false;
-		}
-
-		if (document.querySelectorAll('table[data-tablefiltered]').length === 0) {
-			_log('tablefilter is intended to filter a table with attribute data-tablefiltered - such a table does not currently appear in the DOM');
-		}
-		
 		// type will be input, checkbox or select
 		this.type = _parseType(node);
 		if (!this.type) {
 			return false;
 		}
 
-		// the trigger that will be used to attach the filter event
-		this.trigger = (this.type === 'checkbox' || this.type === 'select') ? 'change' : 'keyup';
 
 		// method will be contain, exclude, min or max
 		this.method = _parseMethod(this, node);
@@ -438,9 +434,7 @@ var tablefilter = (function(){
 			node.setAttribute('data-tablefilter-id', '_' + (counter++) + '_');
 		}
 		this.id = node.getAttribute('data-tablefilter-id');
-		if (this.method === null) {
-			return false;
-		}
+
 		
 		// filter only inside elements with this class
 		this.onClass = node.getAttribute('data-tablefilter-onclass');
@@ -451,28 +445,37 @@ var tablefilter = (function(){
 
 		// filter on the contents of the given attribute, instead of element content
 		this.byAttribute = node.getAttribute('data-tablefilter-byattribute');
-		if (typeof this.byAttribute === 'undefined') {
-			this.byAttribute = null;
+		if (this.byAttribute) {
+			this.isByAttribute = true;
 		}
-		this.isByAttribute = this.byAttribute !== null;
 
+		// keep a reference to our node
 		this.node = node;
+
+		// define trigger function
+		var _this = this;
+		function ontrigger() { 
+			_this.countdown();
+		}
 		
+		// attach the filter event
+		if (this.type === 'checkbox') {
+			if (!this.node.value || !this.node.value.length) {
+				_log('checkbox should have value set to work properly with tablefilter')
+				return false;
+			}
+			this.timeout = 50;
+			this.node.addEventListener('change', ontrigger);
+		} else if (this.type === 'select') {
+			this.timeout = 10;
+			this.node.addEventListener('change', ontrigger);
+			this.node.addEventListener('keyup', ontrigger);
+		} else {
+			this.node.addEventListener('keyup', ontrigger);
+		}
+
 		this.valid = true;
-
-		var self = this;
-		this.ontrigger = function() { self.countdown(); };
-		this.node.addEventListener(this.trigger, this.ontrigger);
 		
-		
-//	    var key = event.keyCode || event.charCode;
-//
-//	    if( key == 8 || key == 46 )
-//	        return false;
-//		this.node.addEventListener('change', function() {_log('change');});
-//		this.node.addEventListener('keydown', function() {_log('keydown');});
-
-
 	}
 	
 	return _init;
